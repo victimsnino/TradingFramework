04342a7f6f46aad84abf6c79bf96ed3cc93ef3ec
Aleksey Loginov <victimsnino@gmail.com>
2024-08-26 00:25:08 +0300

    update coverage

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -1,33 +1,18 @@
-# ---- Variables ----
 
-# We use variables separate from what CTest uses, because those have
-# customization issues
-set(
-    COVERAGE_TRACE_COMMAND
-    lcov -c -q
-    -o "${PROJECT_BINARY_DIR}/coverage.info"
-    -d "${PROJECT_BINARY_DIR}"
-    --include "${PROJECT_SOURCE_DIR}/*"
-    CACHE STRING
-    "; separated command to generate a trace for the 'coverage' target"
-)
-
-set(
-    COVERAGE_HTML_COMMAND
-    genhtml --legend -f -q
-    "${PROJECT_BINARY_DIR}/coverage.info"
-    -p "${PROJECT_SOURCE_DIR}"
-    -o "${PROJECT_BINARY_DIR}/coverage_html"
-    CACHE STRING
-    "; separated command to generate an HTML report for the 'coverage' target"
-)
-
-# ---- Coverage target ----
+file(GLOB_RECURSE COVERAGE_PROFRAW_FILES ${PROJECT_BINARY_DIR} *.profraw)
+file(GLOB_RECURSE COVERAGE_OBJECTS ${PROJECT_BINARY_DIR}/build/bin *)
+set(COVERAGE_OBJECTS_ARGS "")
+foreach(f ${COVERAGE_OBJECTS})
+    list(APPEND COVERAGE_OBJECTS_ARGS -object=${f})
+endforeach()
+set(COVERAGE_ADDITIONAL_ARGS -instr-profile=${PROJECT_BINARY_DIR}/coverage.profdata ${COVERAGE_OBJECTS_ARGS})
 
 add_custom_target(
     coverage
-    COMMAND ${COVERAGE_TRACE_COMMAND}
-    COMMAND ${COVERAGE_HTML_COMMAND}
+    COMMAND llvm-profdata merge -sparse=true ${COVERAGE_PROFRAW_FILES} -o ${PROJECT_BINARY_DIR}/coverage.profdata
+
+    # COMMAND llvm-cov report --ignore-filename-regex=build|tests ${RPP_COVERAGE_TARGETS}
+    COMMAND llvm-cov show --ignore-filename-regex=build|tests --show-branches=count --show-expansions --show-line-counts --show-line-counts-or-regions --show-regions ${COVERAGE_ADDITIONAL_ARGS} > ${PROJECT_BINARY_DIR}/coverage.txt
     COMMENT "Generating coverage report"
     VERBATIM
 )
